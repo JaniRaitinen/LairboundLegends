8d980c59c21816e6daf4895b5a262ec4775f5ea8
teemvat <teemu.vataja@metropolia.fi>
2023-12-04 10:46:35 +0200

    äänet

---
+++ JS/musicPlayer.js
@@ -0,0 +1,153 @@
+
+/* Funktioilla soitetaan musaa. Status = 'play' soittaa, ja status = 'stop' lopettaa.
+   SFX soitetaan esim. sfx.fireball.play();
+*/
+
+
+function menuMusic(status) {
+  Howler.stop()
+    if(status === 'play') {
+         let music1 = new Howl({
+            src: ['Sound/menu-loop.wav'],
+            autoplay: false,
+            loop: true,
+            //volume: (musicVol / 100),
+            onend: function() {
+                console.log("Intro finished!") //For debugging
+            }
+        });
+        let music = new Howl({
+            src: ['Sound/menu-intro.wav'],
+            autoplay: true,
+            loop: false,
+            //volume: (musicVol / 100),
+            onend: function() {
+                console.log("Loop finished!") //For debugging
+                //let TEMP = music1
+                music1 = music
+                //music = TEMP //I had to add this because of a volume slider
+                music.play()
+            }
+        });
+    } else if (status === 'stop'){
+      Howler.stop()
+    }
+}
+
+
+function flightMusic(status) {
+  Howler.stop()
+    if(status === 'play') {
+         let music1 = new Howl({
+            src: ['Sound/flight-loop.wav'],
+            autoplay: false,
+            loop: true,
+            //volume: (musicVol / 100),
+            onend: function() {
+                console.log("Intro finished!") //For debugging
+            }
+        });
+        let music = new Howl({
+            src: ['Sound/flight-intro.wav'],
+            autoplay: true,
+            loop: false,
+            //volume: (musicVol / 100),
+            onend: function() {
+                console.log("Loop finished!") //For debugging
+                //let TEMP = music1
+                music1 = music
+                //music = TEMP //I had to add this because of a volume slider
+                music.play()
+            }
+        });
+    } else if (status === 'stop'){
+      Howler.stop()
+    }
+}
+
+
+function battleMusic(status) {
+  Howler.stop()
+    if(status === 'play') {
+         let music1 = new Howl({
+            src: ['Sound/battle-loop.wav'],
+            autoplay: false,
+            loop: true,
+            //volume: (musicVol / 100),
+            onend: function() {
+                console.log("Intro finished!") //For debugging
+            }
+        });
+        let music = new Howl({
+            src: ['Sound/battle-intro.wav'],
+            autoplay: true,
+            loop: false,
+            //volume: (musicVol / 100),
+            onend: function() {
+                console.log("Loop finished!") //For debugging
+                //let TEMP = music1
+                music1 = music
+                //music = TEMP //I had to add this because of a volume slider
+                music.play()
+            }
+        });
+    } else if (status === 'stop'){
+      Howler.stop()
+    }
+}
+
+
+function mapMusic(status) {
+  Howler.stop()
+    if(status === 'play') {
+         let music1 = new Howl({
+            src: ['Sound/map-loop.wav'],
+            autoplay: false,
+            loop: true,
+            //volume: (musicVol / 100),
+            onend: function() {
+                console.log("Intro finished!") //For debugging
+            }
+        });
+        let music = new Howl({
+            src: ['Sound/map-loop.wav'],
+            autoplay: true,
+            loop: false,
+            //volume: (musicVol / 100),
+            onend: function() {
+                console.log("Loop finished!") //For debugging
+                //let TEMP = music1
+                music1 = music
+                //music = TEMP //I had to add this because of a volume slider
+                music.play()
+            }
+        });
+    } else if (status === 'stop'){
+      Howler.stop()
+    }
+}
+
+
+const sfx = {
+  hover: new Howl({
+    src: 'hover.mp3'
+  }),
+  open: new Howl({
+    src: 'open.mp3'
+  }),
+  close: new Howl({
+    src: 'close.mp3'
+  }),
+  attack: new Howl({
+    src: 'attack.mp3'
+  }),
+  fireball: new Howl({
+    src: 'fireball.mp3'
+  }),
+  potion: new Howl({
+    src: 'potion.mp3'
+  }),
+  start: new Howl({
+    src: 'start.mp3'
+  })
+}

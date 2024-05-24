813b0d46b50edd48823f954d1e2d81aefd67a020
JaniRaitinen <janirait@metropolia.fi>
2024-05-24 20:51:07 +0300

    Siirsin musiikkikoodin gamelogicista musicplayeriin. Lisäsin musicPlayer.js referenssin main.html:ään.

--- FrontendMKII/js/game_logic.js
+++ FrontendMKII/js/game_logic.js
@@ -22,114 +22,9 @@ const playerForm = document.querySelector('#player-form');
 const dialogueBox = document.querySelector('#dialogue-content');
 const tavernModal = document.querySelector('#tavern-modal');
 
-function menuMusic(status) {
-  Howler.stop()
-    if(status === 'play') {
-         let music1 = new Howl({
-            src: ['sound/menu-loop.wav'],
-            autoplay: false,
-            loop: true,
-            onend: function() {
-                console.log("Loop finished!") //For debugging
-            }
-        });
-        let music = new Howl({
-            src: ['sound/menu-intro.wav'],
-            autoplay: true,
-            loop: false,
-            //volume: (musicVol / 100),
-            onend: function() {
-                console.log("Intro finished!") //For debugging
-                music1.play()
-            }
-        });
-    } else if (status === 'stop'){
-      Howler.stop()
-    }
-}
-
-function mapMusic(status) {
-  menuMusic('stop')
-    if(status === 'play') {
-         let music1 = new Howl({
-            src: ['sound/map-loop.wav'],
-            autoplay: false,
-            loop: true,
-            onend: function() {
-                console.log("Intro finished!") //For debugging
-            }
-        });
-        let music = new Howl({
-            src: ['sound/map-loop.wav'],
-            autoplay: true,
-            loop: false,
-            onend: function() {
-                console.log("Loop finished!") //For debugging
-                music.play()
-            }
-        });
-    } else if (status === 'stop'){
-      Howler.stop()
-    }
-}
+menuMusic('play')
 
 
-const sfx = {
-  hover: new Howl({
-    src: 'sound/hover.mp3'
-  }),
-  open: new Howl({
-    src: 'sound/open.mp3'
-  }),
-  close: new Howl({
-    src: 'sound/close.mp3'
-  }),
-  attack: new Howl({
-    src: 'sound/attack.mp3'
-  }),
-  fireball: new Howl({
-    src: 'sound/fireball.mp3'
-  }),
-  potion: new Howl({
-    src: 'sound/potion.mp3'
-  }),
-  start: new Howl({
-    src: 'sound/start.mp3'
-  }),
-  victory: new Howl({
-    src: 'sound/victory.wav'
-  }),
-  lose: new Howl({
-    src: 'sound/lose.wav'
-  }),
-  magic: new Howl({
-    src: 'sound/magic.wav'
-  }),
-  water: new Howl({
-    src: 'sound/water.wav'
-  }),
-  hploss: new Howl({
-    src: 'sound/hploss.wav'
-  }),
-  hammer: new Howl({
-    src: 'sound/hammer.wav'
-  }),
-  sword: new Howl({
-    src: 'sound/sword.wav'
-  }),
-  thunder: new Howl({
-    src: 'sound/thunder.wav'
-  }),
-  ice: new Howl({
-    src: 'sound/ice.wav'
-  }),
-  slash: new Howl({
-    src: 'sound/slash.wav'
-  }),
-  whoosh: new Howl({
-    src: 'sound/whoosh.wav'
-  })
-}
 
 // Backend retrieval function
 async function getData(url) {
@@ -307,8 +202,6 @@ async function updateLairports(url) {
   alert(`${playerName} has lost connection to you... Perhaps you're at fault, too!?`)
   }
 }
-
-menuMusic('play')
 
 document.getElementById('newgamebutton').addEventListener('click', () =>{
   sfx.open.play()
